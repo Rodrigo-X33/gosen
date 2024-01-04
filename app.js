a78153f75304303fa9b335dc87690a9c01338822
Rodrigo-X33 <rodrigo.chaconx33@gmail.com>
2024-01-04 07:47:45 -0600

    Finalizacion de creación del servidor

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/servicios', (req, res)=>{
+    res.sendFile(__dirname + '/servicios.html');
+})
+app.get('/contacto', (req, res)=>{
+    res.sendFile(__dirname + '/contact.html');
+})
+app.listen(process.env.PORT || 3000);
