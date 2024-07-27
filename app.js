c93101a4dba43464037428e244f41eb3ab34d217
Rodrigo-X33 <rodrigo.chaconx33@gmail.com>
2024-07-27 14:50:54 -0600

    reparación de la carpeta public

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
@@ -15,4 +15,4 @@ app.get('/servicios', (req, res)=>{
 app.get('/contacto', (req, res)=>{
     res.sendFile(__dirname + '/contact.html');
 })
-app.listen(process.env.PORT || 3000, ()=>console.log('Server ready on port 3001.'))
+app.listen(process.env.PORT || 3001, ()=>console.log('Server ready on port 3001.'))
